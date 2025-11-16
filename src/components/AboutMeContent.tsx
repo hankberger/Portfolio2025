b93ceae5b9d5a1d776ffd31bb0bd9eb860aafe9a
Hank Berger <hanksberger@gmail.com>
2025-11-15 18:28:38 -0600

    checkpoint

---
+++ src/components/AboutMeContent.tsx
@@ -0,0 +1,3 @@
+export default function AboutMeContent() {
+  return <div></div>;
+}
--- src/components/HankCard.tsx
+++ src/components/HankCard.tsx
@@ -1,6 +1,15 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./styles/HankCard.css";
-import { animate, stagger, splitText, spring, easings, svg } from "animejs";
+import {
+  animate,
+  stagger,
+  splitText,
+  spring,
+  easings,
+  svg,
+  get,
+} from "animejs";
+import ButtonContent from "./ButtonContent";
 
 interface IHankCard {
   scatterCallback: (shouldScatter: boolean) => void;
@@ -8,8 +17,9 @@ interface IHankCard {
 
 export default function HankCard(props: IHankCard) {
   const scatterCallback = props.scatterCallback;
-  let contentHeight = 0;
-  let buttonWidth = 0;
+  let contentHeight = 48;
+  let buttonWidth = 178;
+  const [bigButton, setBigButton] = useState(false);
 
   useEffect(() => {
     const { chars } = splitText("h1", { words: false, chars: true });
@@ -65,17 +75,6 @@ export default function HankCard(props: IHankCard) {
       },
       y: [{ from: ".5rem", to: "0rem", delay: 800 }],
     } as any);
-
-    animate(".chevron .yeet", {
-      // Using translateX instead of x
-      translateX: [
-        { from: 0, to: 150 },
-        { from: 150, to: 0 },
-      ],
-      duration: 2400,
-      ease: easings.eases.inOutCirc,
-      loop: true,
-    } as any);
   }, []);
 
   const toggle = () => {
@@ -85,6 +84,8 @@ export default function HankCard(props: IHankCard) {
 
     const gettinBigger = hankcard.classList.contains("big");
     scatterCallback(gettinBigger);
+    setBigButton(gettinBigger);
+    console.log(bigButton);
 
     if (gettinBigger) {
       contentHeight =
@@ -157,19 +158,7 @@ export default function HankCard(props: IHankCard) {
 
           <div className="content">
             <button onClick={toggle} className="getStarted">
-              Get Started
-              <svg
-                className="chevron"
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 640 640"
-              >
-                <path
-                  className="yeet"
-                  fill="white"
-                  transform=""
-                  d="M212.5 205.5C251.7 172.5 304.6 144 368 144C431.4 144 484.3 172.5 523.5 205.5C562.6 238.5 590.4 277.9 604.5 305.3C609.2 314.5 609.2 325.4 604.5 334.6C590.4 362 562.6 401.4 523.5 434.4C484.3 467.5 431.5 495.9 368 495.9C304.5 495.9 251.7 467.4 212.5 434.4C196.3 420.7 182 405.9 169.8 391.3L80.1 443.6C67.6 450.9 51.7 448.9 41.4 438.7C31.1 428.5 29 412.7 36.1 400.1L82 320L36.2 239.9C29 227.3 31.2 211.5 41.5 201.3C51.8 191.1 67.6 189.1 80.2 196.4L169.9 248.7C182.1 234.1 196.4 219.3 212.6 205.6zM480 320C480 302.3 465.7 288 448 288C430.3 288 416 302.3 416 320C416 337.7 430.3 352 448 352C465.7 352 480 337.7 480 320z"
-                />
-              </svg>
+              <ButtonContent bigButton={bigButton} />
             </button>
           </div>
         </div>
