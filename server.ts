2db40c1de02bb3bab7d1d7a6144616e4d383b742
Hank Berger <hanksberger@gmail.com>
2025-11-16 19:54:00 -0600

    simple web server: :

---
+++ server.ts
@@ -0,0 +1,20 @@
+﻿import express from "express";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+const port = Number(process.env.PORT ?? 3000);
+const distDir = path.resolve(__dirname, "dist");
+
+app.use(express.static(distDir));
+
+app.get("*", (_request, response) => {
+  response.sendFile(path.join(distDir, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Serving dist from ${distDir} on http://localhost:${port}`);
+});
