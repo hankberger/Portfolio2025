f82c2e0516d1f97323e832a7cf77cfad483f589b
Hank Berger <hanksberger@gmail.com>
2026-01-25 16:21:07 -0600

    move camera up on portrait mode

--- src/util/updateCamera.ts
+++ src/util/updateCamera.ts
@@ -2,6 +2,6 @@ import type { PerspectiveCamera } from "three";
 import { isPortrait } from "./isPortrait";
 
 export function updateCamera(camera: PerspectiveCamera) {
-  isPortrait() ? camera.position.set(0, 4, 17) : camera.position.set(-4, 2, 13);
+  isPortrait() ? camera.position.set(0, 6, 17) : camera.position.set(-4, 2, 13);
   camera.rotation.set(-Math.PI / 16, 0, 0);
 }
